9d77a1066531a1a88de5d3ec1f40e7aee57ab64f
ItsmeMing <ngocminh.vu.520@gmail.com>
2022-11-01 16:35:28 +0700

    linh tinh

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Missions from "./components/missions/Missions";
 import Image from "./components/image/Image";
 import About from "./components/about/About";
@@ -5,6 +6,11 @@ import "../main-page/components/slogan/Slogan.scss";
 import "./AboutUs.scss";
 
 const AboutUs = () => {
+    //always start at the top of the page
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        document.title = "About Us - Dossier Perfumes";
+    }, []);
     return (
         <section className="about-us-container g-0">
             <section className="slogan-container container g-0">
